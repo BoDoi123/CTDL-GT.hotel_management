5d19ea85a6992acb3240f678e3d5fb47032900c6
BoDoi123 <chulong_t65@hus.edu.vn>
2023-12-10 00:36:20 +0700

    models and sql code

--- demo/src/main/java/models/calculateprice/SimplePriceCalculator.java
+++ demo/src/main/java/models/calculateprice/SimplePriceCalculator.java
@@ -2,9 +2,21 @@ package models.calculateprice;
 
 import models.Service;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SimplePriceCalculator implements PriceCalculator {
+    private long numberOfDays; // Số ngày đặt phòng
+
+    public SimplePriceCalculator(Date renDate, Date departureDate) {
+        if (departureDate.before(renDate)) {
+            throw new IllegalArgumentException("departureDate must be after renDate");
+        }
+
+        // Tính số ngày thuê
+        this.numberOfDays = TimeUnit.DAYS.convert(departureDate.getTime() - renDate.getTime(), TimeUnit.MICROSECONDS);
+    }
     @Override
     public int calculatePrice(List<Service> services) {
         int totalPrice = 0;
@@ -13,6 +25,6 @@ public class SimplePriceCalculator implements PriceCalculator {
             totalPrice += service.getCost();
         }
 
-        return totalPrice;
+        return totalPrice * (int)numberOfDays;
     }
 }
